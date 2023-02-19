6f21a51964bc0c1d240cefa7b0d7ed4e5c63012f
Yogesh056 <yogiking144@gmail.com>
2023-02-19 21:40:07 +0530

    I have completed prime numbers

---
+++ prime.js
@@ -0,0 +1,12 @@
+let number=5;
+let factor=0;
+
+for(let i=1; i<=number; i++){
+
+if(number%i==1){
+factor++;
+}
+
+}
+
+(factor==2) ? console.log("it is a prime number") : console.log("it is not a prime number");
